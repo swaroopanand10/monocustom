f7ed48b11a8b285eec80d3454d262bc0553491c5
swaroopanand <swaroopanand695@gmail.com>
2022-04-10 00:03:16 +0530

    random

--- lua/monocustom/NvimTree.lua
+++ lua/monocustom/NvimTree.lua
@@ -1,5 +1,5 @@
 local NvimTree = {
-		NvimTreeFolderIcon = {fg = C.blue0, },
+		NvimTreeFolderIcon = {fg = C.folder_blue, },
 		NvimTreeIndentMarker = {fg = "#c5c5c5", },
 		NvimTreeNormal = {fg = C.blue0, bg = C.bg, },
 		NvimTreeVertSplit = {fg = C.blue0, bg = C.bg, },
